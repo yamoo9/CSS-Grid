b026962f92ebb768526cdbda9c16af76b1c956aa
Jee Hoon <yamoo9@naver.com>
2018-07-16 08:23:15 +0900

    postcss-gap-properties 플러그인 추가

---
+++ grid-for-ie/config/postcss-gap-properties.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // 컴파일 과정에서 gap, column-gap, row-gap 속성을 보존할지 유무 설정
+  preserve: true
+};
--- grid-for-ie/config/postcss.config.js
+++ grid-for-ie/config/postcss.config.js
@@ -1,11 +1,13 @@
 const autoprefixer__options = require('./autoprefixer.config');
 const cssnano__options      = require('./cssnano.config');
+const postcssGap__options   = require('./postcss-gap-properties.config');
 
 module.exports = ctx => ({
   parser: ctx.file.extname === '.sss' ? 'sugarss' : false,
   plugins: [
     require("autoprefixer")(autoprefixer__options),
     ctx.env === 'production' && require("cssnano")(cssnano__options),
+    require("postcss-gap-properties")(postcssGap__options)
   ],
   map: false // ctx.env === 'development' ? ctx.options.map : false,
 });
